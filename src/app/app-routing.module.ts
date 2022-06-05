71348c3994b2ff4b2590a97cfabdeebbc4641c69
Camacho <roberto.fernandez@santander.com>
2022-06-05 18:38:14 -0300

    finalization of the project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { AboutComponent } from './components/institutional/about/about.component';
+import { ContactComponent } from './components/institutional/contact/contact.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/navigation/home/home.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/navigation/home/home.component';
+import { MenuComponent } from './components/navigation/menu/menu.component';
+import { FooterComponent } from './components/navigation/footer/footer.component';
+import { ContactComponent } from './components/institutional/contact/contact.component';
+import { AboutComponent } from './components/institutional/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
